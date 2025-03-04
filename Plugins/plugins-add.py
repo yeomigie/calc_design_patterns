596dffce82597d49c84ec3c04647f3574bd678ef
yeomigie <yeo@njit.edu>
2025-03-04 22:40:39 +0500

    Delete Plugins directory

--- Plugins/plugins-add.py
+++
@@ -1,7 +0,0 @@
-class AddCommand:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def execute(self):
-        return self.a + self.b
--- Plugins/plugins-substract.py
+++
@@ -1,7 +0,0 @@
-class SubtractCommand:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def execute(self):
-        return self.a - self.b
